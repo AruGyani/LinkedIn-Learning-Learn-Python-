651c510988b059191dfb7d0ef45a428d70c90d4f
Aru Gyani <aru@gyani.com>
2020-12-10 01:54:45 -0600

    Initial commit.

---
+++ Ch2/helloworld_start.py
@@ -0,0 +1,11 @@
+#
+# Example File for HelloWorld
+#
+
+def main():
+    print("Hello World")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
